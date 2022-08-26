8db9ae1cafd8c7c8b70e9dd535912ce26233ce82
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-08-26 17:16:36 +1000

    Update

--- Discord-Bot/src/event-listners/autocompleate.ts
+++ Discord-Bot/src/event-listners/autocompleate.ts
@@ -3,9 +3,11 @@ import * as discord from 'discord.js';
 async function listner(interaction: discord.AutocompleteInteraction) {
     if (!interaction.isAutocomplete()) return;
 
-    globalThis.config.commands.get(
-        globalThis.config.commandIdMap.get(
-            interaction.commandId)).autocompleate(interaction)
+    try {
+        globalThis.config.commands.get(
+            globalThis.config.commandIdMap.get(
+                interaction.commandId)).autocompleate(interaction)
+    } catch (error) { }
 }
 
 export default {
